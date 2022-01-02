065c286ac612812692a7c219f4dc57cefa3557cf
Andreas Huetter <a.huetter@tricentis.com>
2022-01-02 09:46:58 +0100

    test: add tests to cover everything

--- Java/CogntiveComplexityOnStackoverflow/src/test/java/org/example/CyclomaticComplexityTest.java
+++ Java/CogntiveComplexityOnStackoverflow/src/test/java/org/example/CyclomaticComplexityTest.java
@@ -6,8 +6,30 @@ import static org.assertj.core.api.Assertions.*;
 public class CyclomaticComplexityTest
 {
     @Test
-    public void Foo()
+    public void noFieldSeparatorFound()
     {
-        assertThat(false).isTrue();
+        var sut = new CyclomaticComplexity();
+        assertThat(sut.findDelimiter("")).isEqualTo('\0');
+    }
+
+    @Test
+    public void findsFirstCharacter_forDefaultCase()
+    {
+        var sut = new CyclomaticComplexity();
+        assertThat(sut.findDelimiter("Some word")).isEqualTo('S');
+    }
+
+    @Test
+    public void findsTabFieldSeparator()
+    {
+        var sut = new CyclomaticComplexity();
+        assertThat(sut.findDelimiter("\\t")).isEqualTo('\t');
+    }
+
+    @Test
+    public void findsUnicodeSeparator()
+    {
+        var sut = new CyclomaticComplexity();
+        assertThat(sut.findDelimiter("0")).isEqualTo('\0');
     }
 }
