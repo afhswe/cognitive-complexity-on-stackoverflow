722c5c1530162b5e88282169ba76a1d92a0773b6
Andreas Huetter <a.huetter@tricentis.com>
2022-01-02 10:41:58 +0100

    setup: remove unneccesary subdirectories

---
+++ Java/src/test/java/org/afhswe/SampleATest.java
@@ -0,0 +1,13 @@
+package org.afhswe;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class SampleATest
+{
+    @Test
+    public void Foo()
+    {
+        assertThat(false).isTrue();
+    }
+}
