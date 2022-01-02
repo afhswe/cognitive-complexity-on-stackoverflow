7fa0214bc78d0fa3f2403f00e783e25203edbe35
Andreas Huetter <a.huetter@tricentis.com>
2022-01-02 09:44:07 +0100

    feature: add production code sample with complexity
    test: add test project setup with dummy test

---
+++ Java/CogntiveComplexityOnStackoverflow/src/test/java/org/example/CyclomaticComplexityTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class CyclomaticComplexityTest
+{
+    @Test
+    public void Foo()
+    {
+        assertThat(false).isTrue();
+    }
+}
